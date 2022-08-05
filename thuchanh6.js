314dd99cc59f41a29b57d5e382c99190b5063810
nguyenvantu <nguyenvantu9332@gmail.com>
2022-08-05 14:05:52 +0700

    thuchanh6

---
+++ thuchanh6.js
@@ -0,0 +1,12 @@
+function thuchanh5() {
+    alert('xinchao1!')
+}
+function thuchanh6() {
+    alert('xinchao2!')
+}
+function thuchanh7() {
+    alert('xinchao3!')
+}
+function thuchanh8() {
+    alert('xinchao4')
+}
